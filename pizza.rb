9fe6145ddeb59e2dbbb03e9cc89230b2ab1df52c
Clay Stewart <claytonmstewart@gmail.com>
2013-09-11 14:25:46 -0500

    Add a failing test for Pizza::Topping.initialize that sets whether or not the topping is vegetarian.

--- pizza.rb
+++ pizza.rb
@@ -4,6 +4,15 @@ module Pizza
   end
 
   class Topping
-
+    attr_accessor :name, :vegetarian
+    # Instantiate a new topping.
+    #
+    # name - The String name of the topping.
+    #
+    # Returns a new Topping object.
+    def initialize name, vegetarian=false
+      @name = name
+      @vegetarian = vegetarian
+    end
   end
 end
